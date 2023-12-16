5b9aebcf467e86be98729e1bd4cdc92210aa6c53
Kumar Sunny Suman <kumarsunnysuman2002@gmail.com>
2023-12-17 03:31:27 +0530

    Favicon Added :)

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import "@styles/globals.css";
 import Nav from "@components/nav";
 import Provider from "@components/Provider";
 export const metadata = {
+  icons:{icon:"/assets/images/logo.png"},
+  apple:"/assets/images/logo.png",
   title: "Promptbook",
   description:
     "Discover and share AI-generated prompts for your next writing project.",
@@ -11,9 +13,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <head>
-        <link rel="favicon" href="/assets/images/logo.png" />
         <title>{metadata.title}</title>
-        <link rel="apple-touch-icon" href="public/assets/images/logo.png" />
         <meta name="description" content={metadata.description} />
       </head>
       <body>
