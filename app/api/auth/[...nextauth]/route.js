09cc37736adfc5160871249d59fa3539ba1e920b
Kumar Sunny Suman <kumarsunnysuman2002@gmail.com>
2023-12-20 20:20:16 +0530

    MVP Ready

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,35 +11,32 @@ const handler = NextAuth({
     }),
   ],
   callbacks: {
- 
-
     async session({ session }) {
-        const sessionUser = await User.findOne({ email: session.user.email });
-        session.user.id = sessionUser._id.toString();
-        return session;
-      },
-    
-      async signIn({ profile }) {
-        try {
-          await connectToDB();
-          // check if user already exists
-          const userExists = await User.findOne({ email: profile.email });
-          // If NOT, create a new user
-          if (!userExists) {
-            await User.create({
-              email: profile.email,
-              username: profile.name.replaceAll(/\s/g,'').toLowerCase(),
-              image: profile.picture
-            });
-          }
-          return true;
-        } catch (e) {
-          console.log(e);
-          return false;
-        }
-      },
-}
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
 
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+        // If NOT, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replaceAll(/\s/g, "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- models/user.js
+++ models/user.js
@@ -10,14 +10,16 @@ const userSchema = new Schema({
     type: String,
     required: [true, "Username is requried!"],
     unique: [true, "Username already exists!"],
+
     // match: [
     //   /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
     //   "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
-    // ],
-    image: {
-      type: String
-    },  
+    // ]
+
   },
+  image: {
+    type: String,
+  }
 });
 
 const User = models.User || model("User", userSchema);
